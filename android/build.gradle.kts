3b316afcadba6fc3302cad0693db4bff30711f37
Claas <claas@claas.dev>
2024-07-18 01:03:48 +0200

    Remove old unused stuff from previous attempts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    //TODO might need to set to 8.3.2 if mozilla rust android plugin does not work
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-    id("org.mozilla.rust-android-gradle.rust-android") version "0.9.4" apply false
 }
